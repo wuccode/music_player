01a553c12791f540ffe6bbcae3f2396fdfd4ed16
wuchaung <943757586@qq.com>
2023-03-16 08:25:42 +0800

    edit code

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const express = require('express')
 const {join} = require('path')
 const app = express()
-app.on('request',(req,res)=>{
-	//获取GET参数
-	let {query} = url.parse(req.url,true);
-	//获取post参数
-	let postData='';
-	req.on('data',(chunk)=>{
-		postData+=chunk;
-	});
-	req.on('end',()=>{
-		console.log(querystring.parse(postData));
-	});
-});
-
 app.use(express.static(join(__dirname, 'src')))
 app.use('/api',require('./routes/index.js'));
-app.use('*',(req,res)=>{
+app.use('/',(req,res)=>{
     res.redirect('/app.html?_='+Date.now())
 })
 app.listen(80,()=>{
